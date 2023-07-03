7afe6f94cb8733cbb7ac0fdaab83bf84cc52dd43
Delfina Sol Martinez Pandiani <dmartinezpandiani@gmail.com>
2023-07-03 17:33:57 +0200

    created distributional mosaic KG for Parlamint 2.1 spanish corpus

--- Distributional_MOSAIC/dl_MOSAIC_kg_construction.py
+++ Distributional_MOSAIC/dl_MOSAIC_kg_construction.py
@@ -4,6 +4,12 @@ import os
 from collections import defaultdict
 import textwrap
 import re
+import csv
+import googletrans
+from googletrans import Translator
+
+import csv
+from googletrans import Translator
 
 def generate_clusters_triples():
     # Load data from JSON file
@@ -118,14 +124,89 @@ def clean_sk_eng_csvs(corpus_name, download_date):
 
                     for row in filtered_rows.values():
                         writer.writerow(row[1:])
+
     return output_paths
 
+def translate_corpus_csv(output_paths, corpus_lan):
+    translated_output_paths = []
+    translator = Translator(service_urls=['translate.google.com'])
 
-def generate_collocation_triples(corpus_name, download_date):
+    for file_path in output_paths:
+        # Read the CSV file
+        with open(file_path, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            lines = list(reader)
+
+        # Extract the base name and extension from the input file
+        base_name, extension = os.path.splitext(file_path)
+
+        # Split the base name using '_' as the separator
+        base_name_parts = base_name.split('_')
+
+        # Translate the specific word (e.g., 'comodidad')
+        if base_name_parts[1] == str(download_date + "/libertad"):
+            translated_word = str(download_date + "/freedom")
+        elif base_name_parts[1] == str(download_date + "/seguridad"):
+            translated_word = str(download_date + "/safety")
+        else:
+            translated_word = translator.translate(base_name_parts[1], src=corpus_lan, dest='en').text.lower()
+        #print(base_name_parts[1], "is translated to ", translated_word)
+
+        # Replace the specific word with the translated version
+        base_name_parts[1] = translated_word
+
+        # Recreate the translated base name
+        translated_base_name = '_'.join(base_name_parts)
+
+        # Create the translated output file name
+        output_file = f'{translated_base_name}{extension}'
+        translated_output_paths.append(output_file)
+        # print("NEW OUTPUT PATH", output_file)
+
+        if os.path.exists(output_file):
+            print(f'Translated CSV file "{output_file}" already exists. Skipping translation.')
+            continue
+
+        # Translate the lines
+        translated_lines = []
+        for line in lines:
+            if len(line) >= 2:
+                word = line[1]
+                translation = translator.translate(word, src=corpus_lan, dest='en')
+                translated_word = translation.text.lower()
+
+                # Remove 'to' from verb phrases
+                translated_word_parts = translated_word.split()
+                if len(translated_word_parts) > 1 and translated_word_parts[0].lower() == 'to':
+                    translated_word_parts = translated_word_parts[1:]
+                translated_word = ' '.join(translated_word_parts)
+
+                if len(translated_word_parts) > 1:
+                    translated_word = '-'.join(translated_word_parts)
+
+                translated_line = [line[0], translated_word] + line[2:]
+                translated_lines.append(translated_line)
+
+        # Write the translated lines to the output CSV file
+        with open(output_file, 'w', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(translated_lines)
+
+        print(f'Translated CSV file saved as: {output_file}')
+    print("thjis is the end of output paths", translated_output_paths)
+    return translated_output_paths
+
+
+def generate_collocation_triples(corpus_name, download_date, corpus_lan):
     rdf_prefix = "musco"
-    output_paths = clean_sk_eng_csvs(corpus_name, download_date)
     collocation_triples = []
-
+    print(corpus_lan)
+    if corpus_lan != "en":
+        output_paths = clean_sk_eng_csvs(corpus_name, download_date)
+        output_paths = translate_corpus_csv(output_paths, corpus_lan)
+        # create the translated csvs and better specify the output paths
+    else:
+        output_paths = clean_sk_eng_csvs(corpus_name, download_date)
     for path in output_paths:
         file_name = path.split("/")[-1].replace("_cleaned.csv", "")
         # Extract concept name, corpus name, and download date from the modified file name
@@ -175,7 +256,7 @@ def generate_collocation_triples(corpus_name, download_date):
         collocation_triples.extend(collocate_triples)  # Add triples for the current CSV file to the main list
     return collocation_triples
 
-def create_collocation_situation_kg(corpus_name, download_date, annotator):
+def create_collocation_situation_kg(corpus_name, download_date, annotator, corpus_lan="en"):
     # Prefixes and base declaration
     prefixes = textwrap.dedent("""\
     @prefix : <https://w3id.org/musco#> .
@@ -201,7 +282,7 @@ def create_collocation_situation_kg(corpus_name, download_date, annotator):
     situation_triples = generate_situation_triples(corpus_name, download_date, annotator)
     for triple in situation_triples:
         print(triple)
-    collocation_triples = generate_collocation_triples(corpus_name, download_date)
+    collocation_triples = generate_collocation_triples(corpus_name, download_date, corpus_lan)
     for triple in collocation_triples:
         print(triple)
     combined_set = set(cluster_triples)
@@ -221,10 +302,19 @@ def create_collocation_situation_kg(corpus_name, download_date, annotator):
     return
 
 
+
+
+
+
+
+
+
 # Specifics of a collocation annotation situation
-corpus_name = "brexit-nt"
+corpus_name = "parlaMint21"
+corpus_lan = "es"
 download_date = "20230703"
 annotator = "DelfinaSolMartinezPandiani"
 
 # create that collocation situation's DL KG
-create_collocation_situation_kg(corpus_name, download_date, annotator)
+create_collocation_situation_kg(corpus_name, download_date, annotator, corpus_lan=corpus_lan)
+
